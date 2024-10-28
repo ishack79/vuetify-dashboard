99b861a8668e1cd7a2c61d588639587819e2be8e
Isaac Agustin <isaac.garcia@indra.no>
2024-10-28 17:02:58 +0100

    Added pie chart

--- src/components/AdminDashboard.vue
+++ src/components/AdminDashboard.vue
@@ -2,15 +2,31 @@
 import ChartComponent from './ChartComponent.vue'
 import BarRaceChart from './BarRaceChart.vue'
 import HeatmapChart from './HeatmapChart.vue'
+import PieChart from './PieChart.vue'
 </script>
 
 <template>
-  <v-card class="mx-auto" max-width="1200">
+  <v-card class="mx-auto" max-width="1600">
     <v-card-title>Admin Dashboard</v-card-title>
     <v-card-text>
-      <ChartComponent />
-      <BarRaceChart />
-      <HeatmapChart />
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="6">
+            <ChartComponent />
+          </v-col>
+          <v-col cols="12" md="6">
+            <BarRaceChart />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="6">
+            <HeatmapChart />
+          </v-col>
+          <v-col cols="12" md="6">
+            <PieChart />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-card-text>
   </v-card>
 </template>
---
+++ src/components/PieChart.vue
@@ -0,0 +1,69 @@
+<template>
+    <v-card class="mx-auto" max-width="1200">
+      <v-card-title>Flight and Towing Status</v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col>
+              <v-card>
+                <v-card-text>
+                  <div id="pie-chart" ref="chart" style="height: 400px;"></div>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+    </v-card>
+  </template>
+  
+  <script>
+  import * as echarts from 'echarts';
+  
+  export default {
+    mounted() {
+      this.initChart();
+    },
+    methods: {
+      initChart() {
+        const chart = echarts.init(this.$refs.chart);
+        const options = {
+          title: {
+            text: 'Flight and Towing Status',
+            left: 'center'
+          },
+          tooltip: {
+            trigger: 'item'
+          },
+          legend: {
+            orient: 'vertical',
+            left: 'left'
+          },
+          series: [
+            {
+              name: 'Status',
+              type: 'pie',
+              radius: '50%',
+              data: [
+                { value: 1048, name: 'Departures On Time' },
+                { value: 735, name: 'Departures Delayed' },
+                { value: 580, name: 'Arrivals On Time' },
+                { value: 484, name: 'Arrivals Delayed' },
+                { value: 300, name: 'Towing On Time' },
+                { value: 200, name: 'Towing Delayed' }
+              ],
+              emphasis: {
+                itemStyle: {
+                  shadowBlur: 10,
+                  shadowOffsetX: 0,
+                  shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+              }
+            }
+          ]
+        };
+        chart.setOption(options);
+      }
+    }
+  };
+  </script>
